cea2764a85a9cc7e139fe2bbfd72f4db92c12e9e
Kevin Jolley <serrowpls@gmail.com>
2019-08-27 20:14:45 -0500

    baseline added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+// Checker
 $(document).ready(function() {
   console.log("I'm working!");
+});
+
+// Scroll
+window.addEventListener('scroll', () => {
+  let screenTop = window.scrollY > 30;
+  const topNav = document.getElementById('nav');
+
+  if (screenTop) {
+    topNav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
 });
